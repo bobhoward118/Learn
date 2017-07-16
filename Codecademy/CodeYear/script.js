ac2153aeb8ffcbada1bd6a4cce0c047d5eb10315
bobhoward118 <bobhoward118@gmail.com>
2017-07-15 23:07:37 -0500

    CC update

--- Codecademy/CodeYear/script.js
+++ Codecademy/CodeYear/script.js
@@ -1,3 +1,35 @@
+var myAge = 30;
+console.log(myAge);
+
+"wonderful day".substring(3, 7); // =>
+
+11 % 3 === 0 // => false
+11 % 3 // => 2
+
+if ("Jon".length * 2 / (2 + 1) === 2) {
+  confirm("Will this run the first block?");
+} else {
+  confirm("Or the second block")
+}
+
+if ('Bob Howard'.length < 5) {
+  console.log("Let's go down the first road");
+} else {
+  console.log("You have a long name");
+}
+
+if ('Bob'.length >= 3) {
+  console.log('Your name is at least 3 letters long');
+}
+
+!== // Not equal to
+=== // Equal to
+<= // Less than or equal to
+>= // Greater than or equal to
+< // Less than
+> // Greater than
+
+console.log(15 > 4);
 console.log(2 * 5);
 "I'm coding like a champ".length > 10 // => true
 prompt('Where are you from?');
