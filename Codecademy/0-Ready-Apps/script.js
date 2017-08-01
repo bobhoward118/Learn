e692d31fc2b8454e1b4ca500f5d44f2e8fd92d43
bobhoward118 <bobhoward118@gmail.com>
2017-07-31 22:56:20 -0500

    CC update

--- Codecademy/0-Ready-Apps/script.js
+++ Codecademy/0-Ready-Apps/script.js
@@ -1,26 +1,31 @@
-Math.ceil(43.8); // => 44
-Math.floor(Math.random() * 50); // => 36
-Math.random() * 50; // => 17 digits
+Math.ceil(43.8); // 44
+Math.floor(Math.random() * 50); // 36
+Math.random() * 50; // 17 digits
 
 
-'Bob Howard'.split('o'); // => ["B", "b H", "ward"]
-'Bob Howard'.slice(4, 7); // => "How"
-  'Bob Howard'.slice(4); // => "Howard"
-'Bob Howard'.search('Ho'); // => 4 (index of match, -1 no match)
-'Bob Howard'.replace('Bob', 'Joe'); // => "Joe Howard"
+'Bob'.charAt(2); // "b"
+'Bob'.concat(' Paul ', 'Howard'); // "Bob Howard"
+'Bob Howard'.endsWith('rd'); // true
+  'Bob Howard'.endsWith('ob', 3); // true
+'Bob Howard'.includes('Bob'); // true
+  'Bob Howard'.includes('Bob', 1); // false (0 is true)
+'Bob Howard'.indexOf('H'); // 4  {first occurance}
+  'Bob Howard'.indexOf('H', 5); // -1  (4 would return 4)
+'Bob Howard'.lastIndexOf('o'); // 5  {first occurance
+'Bob'.localeCompare('Bo'); // 1 (1-before, 0-same, -1-after)
+'Bob'.padEnd(6, 'x'); // "Bobxxx"
+'Bob'.padStart(6, 'abc'); // 'abcBob'
 'Bob'.repeat(3); // => 'BobBobBob'
-'Bob'.padStart(6, 'abc'); // => 'abcBob'
-'Bob'.padEnd(6, 'x'); // => "Bobxxx"
-'Bob'.localeCompare('Bo'); // => 1 (1-before, 0-same, -1-after)
-'Bob Howard'.lastIndexOf('o'); // => 5  {first occurance
-'Bob Howard'.indexOf('H'); // => 4  {first occurance}
-'Bob Howard'.endsWith('rd'); // => true
-'Bob Howard'.includes('Bob'); // => true
-'Bob'.concat(' Howard'); // => "Bob Howard"
-'Bob'.charAt(2); // => "b"
-'  Bob  '.trim(); // => "Bob"
-'Bob'.startsWith('B'); // => true
-'Bob'.toUpperCase(); // => "BOB"
-'Bob'.length; // => 3
+'Bob Howard'.replace('Bob', 'Joe'); // "Joe Howard"
+'Bob Howard'.search('Ho'); // 4 (index of match, -1 no match)
+'Bob Howard'.slice(4); // "Howard"
+'Bob Howard'.slice(4, 7); // "How"
+'Bob Howard'.split('o'); // ["B", "b H", "ward"]
+'Bob'.startsWith('B'); // true
+'Bob'.toUpperCase(); // "BOB"
+'  Bob  '.trim(); // "Bob"
+
+
+'Bob'.length; // 3
 console.log(3.5 + 25);
 console.log('Greater');
