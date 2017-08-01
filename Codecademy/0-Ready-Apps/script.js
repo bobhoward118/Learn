3c9da9d370482a75b1326cbde2dfac13872e4cab
bobhoward118 <bobhoward118@gmail.com>
2017-08-01 00:17:36 -0500

    update

--- Codecademy/0-Ready-Apps/script.js
+++ Codecademy/0-Ready-Apps/script.js
@@ -13,13 +13,13 @@ Math.random() * 50; // 17 digits
   'Bob Howard'.indexOf('H', 5); // -1  (4 would return 4)
 'Bob Howard'.lastIndexOf('o'); // 5  {first occurance
 'Bob'.localeCompare('Bo'); // 1 (1-before, 0-same, -1-after)
-'Bob'.padEnd(6, 'x'); // "Bobxxx"
+'Bob'.padEnd(6, 'xo'); // "Bobxox"
 'Bob'.padStart(6, 'abc'); // 'abcBob'
 'Bob'.repeat(3); // => 'BobBobBob'
-'Bob Howard'.replace('Bob', 'Joe'); // "Joe Howard"
-'Bob Howard'.search('Ho'); // 4 (index of match, -1 no match)
+'Bob Howard Bob'.replace('Bob', 'Joe'); // "Joe Howard"
+'Bob Howard'.search('regexp'); // 4 (index of match, -1 no match)
 'Bob Howard'.slice(4); // "Howard"
-'Bob Howard'.slice(4, 7); // "How"
+  'Bob Howard'.slice(4, 7); // "How"
 'Bob Howard'.split('o'); // ["B", "b H", "ward"]
 'Bob'.startsWith('B'); // true
 'Bob'.toUpperCase(); // "BOB"
